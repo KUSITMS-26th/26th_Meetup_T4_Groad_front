256e539cbd3af2d57d914f7051f2a08e9b7b79d5
Dev_KimKU <86593582+KimKU1018@users.noreply.github.com>
2022-11-24 18:32:37 +0900

    Create SliderContent.js

---
+++ src/pages/main/SliderContent.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './SliderContent.scss'
+
+function SliderContent({activeIndex,imgSlider}){
+  return(
+    <section>
+      <Link id="ProductBigCategory" to="/product/4">
+      {imgSlider.map((slide,index)=>(
+        <div key={index} 
+        className={index === activeIndex
+        ? "slides active"
+        : "inactive"}>
+          <img className="slide-img" src={slide.urls} alt="img"/>
+          <h2 className="slide-title">{slide.title}</h2>
+          <p className="slide-text">{slide.text}</p>
+        </div>
+      ))}
+      </Link>
+    </section>
+  )
+}
+export default SliderContent;
